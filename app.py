e0d3297a4e4a5be0489017563684d8761cf0d950
WillChen <will.chen936@gmail.com>
2023-12-04 00:35:21 +0800

    feat: Login hadling
    Check the username & password than redirect to error, manager or
    customer page.
    
    Issue:

--- app.py
+++ app.py
@@ -6,6 +6,17 @@ try:
     print("Pinged your deployment. You successfully connected to MongoDB!")
 except Exception as e:
     print(e)
+db = client.order_system
+collection_user = db.user
+result = collection_user.find_one({
+    "username": "Manager"
+})
+if result == None:
+    collection_user.insert_one({
+        "username": "Manager",
+        "password": "manager123",
+        "permission": "admin"
+    })
 
 
 from flask import Flask
@@ -18,6 +29,7 @@ app = Flask(
     static_folder = "public",
     static_url_path="/"
 )
+
 app.secret_key = "root123"
 
 @app.route("/")
@@ -28,16 +40,47 @@ def Index():
 def Login():
     username = request.form["username"]
     password = request.form["password"]
-    return "Login Page"
+
+    if username == "" or password == "":
+        return redirect("/error?msg=Please fill in the username or password")
+    
+    doc = collection_user.find_one({
+        "$and":[
+            {"username": username},
+            {"password": password}
+        ]
+    })
+    if not doc:
+        return redirect("/error?msg=Username or Password is wrong")
+    
+    if username == "Manager":
+        return redirect("/manager")
+    else:
+        return redirect("/customer")
+    
 
 @app.route("/signup", methods = ["POST"])
 def Signup():
+    username = request.form["username"]
+    password = request.form["password"]
+
+    if username == "" or password == "":
+        return redirect("/error?msg=Please fill in the username or password")
+    
     return "Signup Page"
 
 @app.route("/error")
 def Error():
     msg = request.args.get("msg", "There's a error occurred, pls contact with the service dept.")
     return render_template("error.html", message = msg)
+
+@app.route("/manager")
+def Manager():
+    return render_template("manager.html")
+
+@app.route("/customer")
+def Customer():
+    return render_template("customer.html")
 
 
 app.run(port=3000)
